d2d5e62b0eb8911f88db3d471b079815315d1f3d
Luis <58019574+ludev84@users.noreply.github.com>
2025-07-16 02:05:03 -0600

    1. Add models

--- auctions/models.py
+++ auctions/models.py
@@ -3,18 +3,37 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    id = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return self.username
 
 
 class Listing(models.Model):
-    title = models.CharField(max_length=250)
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=64)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Bid(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    offer = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.user.username} offers {self.offer} dollars for {self.listing}."
 
 
 class Comment(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    content = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"Comment by {self.user.username}"
